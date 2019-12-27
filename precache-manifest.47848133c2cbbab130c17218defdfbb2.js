19fc70dc779218f3f6aa7b7b027441eed4209ae0
Jonathan Moniz <monizj@pepperlaw.com>
2019-12-27 08:51:49 -0500

    Updates

---
+++ precache-manifest.47848133c2cbbab130c17218defdfbb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2e439fee8fa77fc648ad1ca0ecb793",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "0874390bca2a12ae182f",
+    "url": "/monstersrolodex/static/css/main.6a8e593b.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monstersrolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersrolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0874390bca2a12ae182f",
+    "url": "/monstersrolodex/static/js/main.a3547cbe.chunk.js"
+  },
+  {
+    "revision": "7b905085007ee6449784",
+    "url": "/monstersrolodex/static/js/runtime-main.a95d1390.js"
+  }
+]);
